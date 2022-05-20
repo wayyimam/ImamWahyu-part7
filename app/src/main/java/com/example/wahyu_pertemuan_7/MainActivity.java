375ae1a9e88e02aedd0d01037d24559caa3dc321
Syaugi1008 <101582404+Syaugi1008@users.noreply.github.com>
2022-05-20 15:44:30 +0700

    commit

---
+++ app/src/main/java/com/example/wahyu_pertemuan_7/MainActivity.java
@@ -0,0 +1,45 @@
+package com.example.wahyu_pertemuan_7;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.os.Bundle;
+import android.provider.MediaStore;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private static final int CAMERA_REQUEST = 1888;
+    ImageView imageView;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        imageView = findViewById(R.id.imageCamera);
+        Button button = findViewById(R.id.buttonCamera);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intentCamera = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                startActivityForResult(intentCamera, CAMERA_REQUEST);
+            }
+        });
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == CAMERA_REQUEST && resultCode == RESULT_OK) {
+            Bitmap photoCamera = (Bitmap) data.getExtras().get("data");
+            imageView.setImageBitmap(photoCamera);
+        }
+    }
+}
